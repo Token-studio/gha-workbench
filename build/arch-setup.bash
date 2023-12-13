3f2595a840ecec7e9e98ce9b7a661319af40fae4
Fragment <psn8888@linux.com>
2023-12-14 00:37:40 +0900

    use arch-install-scripts

--- build/arch-setup.bash
+++ build/arch-setup.bash
@@ -3,5 +3,5 @@
 cat << EOF > /etc/pacman.d/mirrorlist
 Server = https://cloudflaremirrors.com/archlinux/\$repo/os/\$arch
 EOF
-
+pacman -Syu --noconfirm arch-install-scripts
 pacstrap ./build/root.x86_64 base base-devel linux-hardened linux-hardened-headers python
