44a7a71580135ea4aca739e66541fb4da8063ed9
Fragment <psn8888@linux.com>
2024-06-23 17:30:23 +0900

    update build script

--- fetch/verify.sh
+++ fetch/verify.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+readonly IMAGE_FILE="archlinux-bootstrap-x86_64.tar.zst"
+readonly IMAGE_SIGNATURE_FILE="archlinux-bootstrap-x86_64.tar.zst.sig"
+readonly SIGNER_FILE="release-key.pgp"
+
 apk add sequoia-sq
-sq network wkd fetch pierre@archlinux.org -o release-key.pgp && echo "OK: fetched key"
-sq verify --signer-file release-key.pgp --detached archlinux-bootstrap-x86_64.tar.gz.sig archlinux-bootstrap-x86_64.tar.gz
+sq network wkd fetch pierre@archlinux.org -o ${SIGNER_FILE}
+sq verify --signer-file ${SIGNER_FILE} --detached ${IMAGE_SIGNATURE_FILE} ${IMAGE_FILE}
