b4c3140b20dee4bf03a9c779d28ddb5ea1614cfa
Fragment <psn8888@linux.com>
2024-06-23 17:19:44 +0900

    update build script

--- fetch/verify.sh
+++ fetch/verify.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 apk add sequoia-sq
-echo "OK: sq installed"
-sq network wkd fetch pierre@archlinux.org -o release-key.pgp
+sq network wkd fetch pierre@archlinux.org -o release-key.pgp && echo "OK: fetched key"
 sq verify --signer-file release-key.pgp --detached archlinux-bootstrap-x86_64.tar.gz.sig archlinux-bootstrap-x86_64.tar.gz
